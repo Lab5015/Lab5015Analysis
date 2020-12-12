29bf56788751ddb2d3794864ed198bea2ae89683
abenagli <andrea.benaglia@cern.ch>
2020-12-12 16:31:32 +0100

    Co60 coincidence analizer program

--- scripts/submit_moduleCharacterization.py
+++ scripts/submit_moduleCharacterization.py
@@ -9,92 +9,122 @@ import subprocess
 parser = argparse.ArgumentParser(description='This script splits moduleCharacterization_step* tasks in multiple parallel jobs')
 
 parser.add_argument("-l",  "--label",          required=True, type=str, help="job label")
-parser.add_argument("-f",  "--fileFolder",          required=True, type=str, help="file folder")
+parser.add_argument("-i",  "--inputFolder",     required=True, type=str, help="input file folder")
+parser.add_argument("-o",  "--outputFolder",     required=True, type=str, help="output file folder")
 parser.add_argument("-b",  "--baseFolder",     required=True, type=str, help="base folder")
 parser.add_argument("-e",  "--exeName",        required=True, type=str, help="absolute path of executable")
 parser.add_argument("-r",  "--runs",           required=True, type=str, help="comma-separated list of runs to be processed")
 parser.add_argument("-c",  "--configFile",     required=True, type=str, help="config file")
-parser.add_argument("-s",  "--submit",                                  help="submit jobs", action='store_true')
-parser.add_argument("-v",  "--verbose",                                 help="increase output verbosity", action='store_true')
+parser.add_argument("-s",  "--submit",         													help="submit jobs", action='store_true')
+parser.add_argument("-v",  "--verbose",        													help="increase output verbosity", action='store_true')
+parser.add_argument("-p",  "--numParal", 			 required=True, type=int, help='number of parallel jobs, 0 for all')
 
 args = parser.parse_args()
 
+preruns = []
 
 runs = []
 comma_list = args.runs.split(',')
 for item in comma_list:
-   hyphen_list = item.split('-')
-   if len(hyphen_list) > 1:
-      for i in range(int(hyphen_list[0]), int(hyphen_list[1])+1):
-         runs.append(i)
-   else:
-      runs.append(int(hyphen_list[0]))
-print runs
+	hyphen_list = item.split('-')
+	if len(hyphen_list) > 1:
+		for i in range(int(hyphen_list[0]), int(hyphen_list[1])+1):
+			preruns.append(i)
+	else:
+		preruns.append(int(hyphen_list[0]))
 
+if args.numParal == 0:
+	listRuns= []
+	for i in range (len(preruns)):
+		listRuns.append(int(preruns[i]))
+	runs.append(listRuns);
+
+if args.numParal !=0:
+	step = (int)(len(preruns)/args.numParal)
+	
+	for i in range (step): 
+		listRuns= []
+		for j in range ( args.numParal * i , args.numParal * (i+1)):
+			listRuns.append(int(preruns[j]))
+		runs.append(listRuns)
+		del listRuns
+	
+	if len(preruns) % args.numParal != 0:
+		listRuns= []
+		for j in range ( step * args.numParal, len(preruns)):
+			listRuns.append(int(preruns[j]))
+		runs.append(listRuns)
+		
+			
+print runs
 
 print 
 print 'START'
 print 
 
-currDir = os.getcwd()
-
-print
-
-try:
-   subprocess.check_output(['mkdir','jobs'])
-except subprocess.CalledProcessError as e:
-   print e.output
-try:
-   subprocess.check_output(['mkdir','jobs/'+args.label])
-except subprocess.CalledProcessError as e:
-   print e.output
 
 
-parallelCommand = "parallel --results ./jobs/"+args.label+"/ "+args.baseFolder+"/"+args.exeName+" ::: "
+for i in range (len(runs)):
+	currDir = os.getcwd()
+
+	print
+
+	try:
+	   subprocess.check_output(['mkdir','jobs'])
+	except subprocess.CalledProcessError as e:
+	   print e.output
+	try:
+	   subprocess.check_output(['mkdir','jobs/'+args.label])
+	except subprocess.CalledProcessError as e:
+	   print e.output
 
 
-##### creates directory and file list for job #######
-jobDir = currDir+'/jobs/'+args.label+'/'
-os.system('mkdir '+jobDir)
-os.chdir(jobDir)
+	parallelCommand = "parallel --results ./jobs/"+args.label+"/ "+args.baseFolder+"/"+args.exeName+" ::: "
 
-for run in runs:
-   ##### creates config file #######
-   with open(args.baseFolder+'/'+args.configFile) as fi:
-      contents = fi.read()
-      configFileName = jobDir+"/config_run"+str(run)+".cfg"
-      with open(configFileName, "w") as fo:
-         fo.write(contents)
-      command = 'sed -i \"s%^runs .*$%runs '+str(run)+'%\" '+configFileName
-      os.system(command)
-      command = 'sed -i \"s%^step1FileName .*$%step1FileName '+args.fileFolder+'/plots/moduleCharacterization_step1_run'+str(run)+'.root%\" '+configFileName
-      os.system(command)
-      command = 'sed -i \"s%^outFileNameStep1 .*$%outFileNameStep1 '+args.baseFolder+'/plots/moduleCharacterization_step1_run'+str(run)+'.root%\" '+configFileName
-      os.system(command)
-      command = 'sed -i \"s%^outFileNameStep2 .*$%outFileNameStep2 '+args.baseFolder+'/plots/moduleCharacterization_step2_run'+str(run)+'.root%\" '+configFileName
-      os.system(command)
+
+	##### creates directory and file list for job #######
+	jobDir = args.baseFolder+'/jobs/'+args.label+'/'
+	os.system('mkdir '+jobDir)
+	os.chdir(jobDir)
+	for run in runs[i]:
+	##### creates config file #######
+		with open(args.baseFolder + '/'+args.configFile) as fi:
+			contents = fi.read()
+			configFileName = jobDir+"/config_run"+str(run)+".cfg"
+			with open(configFileName, "w") as fo:
+				fo.write(contents)
+			command = 'sed -i \"s%^runs .*$%runs '+str(run)+'%\" '+configFileName
+			os.system(command)
+			command = 'sed -i \"s%^step1FileName .*$%step1FileName '+args.inputFolder+'/plots/moduleCharacterization_step1_run'+str(run)+'.root%\" '+configFileName
+			os.system(command)
+			command = 'sed -i \"s%^outFileNameStep1 .*$%outFileNameStep1 '+args.outputFolder+'/plots/moduleCharacterization_step1_run'+str(run)+'.root%\" '+configFileName
+			os.system(command)
+			command = 'sed -i \"s%^outFileNameStep2 .*$%outFileNameStep2 '+args.outputFolder+'/plots/moduleCharacterization_step2_run'+str(run)+'.root%\" '+configFileName
+			os.system(command)
+			#command = 'sed -i \"s%^plotDir .*$%plotDir '+'/var/www/html/ModuleCharacterization/ALIO/Co60/FBK_thinQuartz/Data/'+str(run)+ '%\" '+configFileName
+			#os.system(command)
       
-   parallelCommand += configFileName + " "
+		parallelCommand += configFileName + " "
 
 
-##### creates job file #######
-with open(jobDir+'/jobs.sh', 'w') as fout:
-   fout.write("#!/bin/sh\n")
-   fout.write("echo\n")
-   fout.write("echo 'START---------------'\n")
-   fout.write("echo 'current dir: ' ${PWD}\n")
-   fout.write("cd "+str(args.baseFolder)+"\n")
-   fout.write("echo 'current dir: ' ${PWD}\n")
-   fout.write("source scripts/setup.sh\n")
-   fout.write(parallelCommand+"\n")
-   fout.write("echo 'STOP---------------'\n")
-   fout.write("echo\n")
-   fout.write("echo\n")
-   os.system("chmod 755 job.sh")
+	##### creates job file #######
+	with open(jobDir+'/jobs.sh', 'w') as fout:
+	   fout.write("#!/bin/sh\n")
+	   fout.write("echo\n")
+	   fout.write("echo 'START---------------'\n")
+	   fout.write("echo 'current dir: ' ${PWD}\n")
+	   fout.write("cd "+str(args.baseFolder)+"\n")
+	   fout.write("echo 'current dir: ' ${PWD}\n")
+	   fout.write("source scripts/setup.sh\n")
+	   fout.write(parallelCommand+"\n")
+	   fout.write("echo 'STOP---------------'\n")
+	   fout.write("echo\n")
+	   fout.write("echo\n")
+	   os.system("chmod 755 job.sh")
    
-if args.submit:
-   os.system("source "+jobDir+"/jobs.sh")
-
+	if args.submit:
+  	 os.system("source "+jobDir+"/jobs.sh")
+	print 'end job'
 print
 print 'END'
 print
--- src/Co60SpectrumAnalyzer.cc
+++ src/Co60SpectrumAnalyzer.cc
@@ -6,8 +6,7 @@ std::map<std::string,std::pair<float,float> > Co60SpectrumAnalyzer(TH1F* histo,
                                                                    std::vector<float>* ranges)
 {
   gStyle -> SetOptFit(0);
-  
-  
+  histo->GetXaxis()->SetRangeUser(0,50);
   std::map<std::string,std::pair<float,float> > res;
   
 
@@ -17,12 +16,12 @@ std::map<std::string,std::pair<float,float> > Co60SpectrumAnalyzer(TH1F* histo,
   while ( histo -> GetBinContent(startBin) < 100 && histo -> GetBinContent(startBin-1) < 100 ){
 	startBin ++;
   }
- 
-  endBin = startBin;
-  while ( histo -> GetBinContent(endBin) > 2 && histo -> GetBinContent(endBin-1) > 2 && histo -> GetBinContent(endBin+1) > 2){
+
+  endBin =  histo->GetNbinsX() /2.;
+  while ( histo -> GetBinContent(endBin) < 50 && histo -> GetBinContent(endBin-1) < 50 && histo -> GetBinContent(endBin+1) < 50 && endBin < histo->GetNbinsX() -50){
 		endBin++;
   }
-
+	
   //Derivative calculation	
   std::vector<double> derivative;
   derivative.push_back(0);
@@ -30,19 +29,17 @@ std::map<std::string,std::pair<float,float> > Co60SpectrumAnalyzer(TH1F* histo,
   	if ( bin < startBin){
 		derivative.push_back(0);
 	
-	}
-	if( bin >= startBin){	
-	
-		double difference = histo->GetBinContent(bin+1) - histo->GetBinContent(bin);
-		double distance =  histo->GetBinCenter(bin+1) - histo->GetBinCenter(bin);		
-		derivative.push_back(difference/distance);		
-	}
+		}
+		if( bin >= startBin){	
+			double difference = histo->GetBinContent(bin+1) - histo->GetBinContent(bin);
+			double distance =  histo->GetBinCenter(bin+1) - histo->GetBinCenter(bin);		
+			derivative.push_back(difference/distance);		
+		}
   }
-
+	 
   //Looking for the right side of the 1332 Peak
   std::vector <double> rangesDerivative;
   double drop = startBin+1;
-  
   int counter = 0;
   double sum = 0;
   for ( int j = startBin; j < endBin; j++){
@@ -53,152 +50,155 @@ std::map<std::string,std::pair<float,float> > Co60SpectrumAnalyzer(TH1F* histo,
 			rangesDerivative.push_back(double(sum)/5.);
 			counter = 0;
 			sum = 0;	
+			
 		}
   }
-  
-  int check = 0;
-  
-  while ( check != 99){
-	double min = *std::min_element( rangesDerivative.begin(), rangesDerivative.end());
-	int minIndex = 5;
-	do{minIndex ++;}
-		while(rangesDerivative[minIndex] != min);
-		
-	check =  99;
 
-	for ( int i = minIndex +1;  i < minIndex + 5; i++){
-		if( rangesDerivative[i] > 0){
+  int check = 0;
+  std::vector <int> erasedIndex;
+	erasedIndex.push_back(0);
+	erasedIndex.push_back(rangesDerivative.size());
+	int minIndex = 5;
+	double min;
+  while ( check != 99){
+		min = 10000;
+		for( int j = 0; j < erasedIndex.size()-1; j++){
+			for ( int i = erasedIndex[j] +1; i < erasedIndex[j+1] ; i++){
+				if(rangesDerivative[i] < min){
+					min = rangesDerivative[i];
+					minIndex = i;
+				}
+			}
+		}
+		check =  99;
+		int risalite = 0;
+		int esclusioni = 0;
+		for ( int i = minIndex -1;  i < minIndex + 4; i++){
+			if( rangesDerivative[i] > 0){
+				risalite ++;
+				if( rangesDerivative[i] > 160){
+					esclusioni ++;
+				}
+			}
+		}
+		if( risalite > 1 || esclusioni > 0){
 			check = 0;
 		}
+		if (check == 0){
+			erasedIndex.push_back(minIndex);
+		}
+		sort(erasedIndex.begin(), erasedIndex.end());	
+		drop = histo -> GetBinCenter(startBin + (minIndex+5) *5);
 	}
-	if (check == 0){
-		rangesDerivative.erase(rangesDerivative.begin() + minIndex);
-	}
-	drop = histo -> GetBinCenter(startBin + (minIndex+5) *5);
+	std::vector<double> totalPeaks;
+	std::vector<double> realPeaks;
 	
- }
+	TF1* fitFunc;
 	
 
-  //Searching 1173 peak
-  int nPeaks = 4;
-  TSpectrum* spectrum = new TSpectrum(nPeaks);
-
-  histo -> GetXaxis() -> SetRangeUser(0., drop);
-                                                                                                                                                                                                                                                                                                                                                                             
-  int nFound = spectrum -> Search(histo, 0.01, "", 0.001);		
-  double* peaks = spectrum -> GetPositionX();			
+	if (drop > histo->GetBinCenter((endBin-startBin)/3.) ){
+		//Searching 1173 and 1332 peaks
+		int nPeaks = 4;
+		int nFound = 0;
+		TSpectrum* spectrum = new TSpectrum(nPeaks);
+		float start =  histo->GetBinCenter(startBin);
+		float stop =   histo->GetBinCenter(endBin);
+		histo -> GetXaxis() -> SetRangeUser(start, stop);
+	                                                                                                                                                                                                                                                                                                                                                                           
+		nFound = spectrum -> Search(histo, 1,"" , 0.001);
+		double* peaks = spectrum -> GetPositionX();
+		int index = -1;
+		if (nFound > 1){
+			for( int i = 0; i < nFound; i++){
+				totalPeaks.push_back(peaks[i]);
+			}
+			std::sort(totalPeaks.begin(),totalPeaks.end());
+			for( int i=0; i< totalPeaks.size(); i++){
+				if( totalPeaks[i] < drop){
+					index ++;
+				}
+			}
+			int a;
+			int b;
+			if( histo -> GetBinContent( histo -> FindBin(totalPeaks[index])) > histo -> GetBinContent( histo -> FindBin(totalPeaks[index-1]))){
+				a = index;
+				b = index -1;
+			}
+			if( histo -> GetBinContent( histo -> FindBin(totalPeaks[index])) < histo -> GetBinContent( histo -> FindBin(totalPeaks[index-1]))){
+				b = index;
+				a = index -1;
+			}
  
-  std::vector<double> totalPeaks;
-  std::vector<double> totalPeaks2;
-  std::vector<double> realPeaks;
-
-
-  if(nFound >1){
-	for( int i = 0; i < nFound; i++){
-		totalPeaks.push_back(peaks[i]);
- 	}	
-  
-  	sort(totalPeaks.begin(), totalPeaks.end());
-	
-	for( int j = 0; j < totalPeaks.size(); j++){
-	}	
-  }
-
-  if(totalPeaks.size() == 4){
-  	realPeaks.push_back(totalPeaks[totalPeaks.size()-2]);
-	realPeaks.push_back(totalPeaks[totalPeaks.size()-1]);
-  }
-  if(totalPeaks.size() <4){
-	double d1 = drop - histo->GetBinWidth(10)*20 -totalPeaks[totalPeaks.size()-1];
-	TF1* fitFunc_gaus = new TF1("fitFunc_gaus","gaus",0.95*totalPeaks[2],1.05*totalPeaks[2]);
-	histo -> Fit(fitFunc_gaus,"QR"); 
-	double sigma = fitFunc_gaus -> GetParameter(2);
-	if(d1 > sigma*1.5){
-		realPeaks.push_back(totalPeaks[totalPeaks.size()-1]);
+			if( histo -> GetBinContent( histo -> FindBin(totalPeaks[a])) / histo -> GetBinContent( histo -> FindBin(totalPeaks[b])) < 10){
+			realPeaks.push_back(totalPeaks[index-1]);
+			realPeaks.push_back(totalPeaks[index]);
+			}
+			if( histo -> GetBinContent( histo -> FindBin(totalPeaks[a])) / histo -> GetBinContent( histo -> FindBin(totalPeaks[b])) > 10){
+			realPeaks.push_back(totalPeaks[index-2]);
+			realPeaks.push_back(totalPeaks[index-1]);
+			}
+			
+		}
 	}
-	if(d1 < sigma*1.5){
-		realPeaks.push_back(totalPeaks[totalPeaks.size()-2]);
-		realPeaks.push_back(totalPeaks[totalPeaks.size()-1]);
-	}
-  }
-
- 
-
-  //Fitting 1173 keV peak
-  if (realPeaks.size() == 1 || realPeaks.size() == 2 ){
-  	TF1* fitFunc_1173 = new TF1("fitFunc_1173","gaus",0.95*realPeaks[0],1.05*realPeaks[0]);
-	fitFunc_1173 -> SetLineColor(kBlack);
-	fitFunc_1173 -> SetLineWidth(2);
-	histo -> Fit(fitFunc_1173,"QRS+"); 
-	  
-	res["1.173 MeV"] = std::make_pair(fitFunc_1173->GetParameter(1),fitFunc_1173->GetParameter(2));
-	//histo -> GetXaxis() -> SetRangeUser(0.,3.*fitFunc_1173->GetParameter(1));			
-  }
-
-  //Searching 1332 peaks if previously failed
-  if(realPeaks.size() == 1){
-	  histo -> GetXaxis() -> SetRangeUser ( res["1.173 MeV"].first +res["1.173 MeV"].second, 40.);
-	  nPeaks = 2;
-	  TSpectrum* spectrum2 = new TSpectrum(nPeaks);
-	  nFound = spectrum2 -> Search(histo, 0.01, "", 0.001);		
-	  double* peaks2 = spectrum2 -> GetPositionX();			
-
-	  if(nFound >0){
-		for( int i = 0; i < nFound; i++){
-			totalPeaks2.push_back(peaks2[i]);
-	 	}	
-
-	  	sort(totalPeaks2.begin(), totalPeaks2.end());	
-	
-	  	realPeaks.push_back(totalPeaks2[0]);
-	  }
-  }
-  
-  histo -> GetXaxis() -> SetRangeUser(0., 40.);
-  
+	histo -> GetXaxis() -> SetRangeUser(0, 50);
+	if (realPeaks.size() == 1 || realPeaks.size() == 2 ){
+			fitFunc = new TF1( "fitFunc", "gaus", realPeaks[0] -0.5, realPeaks[0] +0.5);
+			histo-> Fit(fitFunc, "NQR");
+			TF1* fitFunc_1173 = new TF1("fitFunc_1173","gaus",fitFunc->GetParameter(1)-fitFunc->GetParameter(2)*0.75, fitFunc->GetParameter(1)+fitFunc->GetParameter(2)*0.5);
+			fitFunc_1173 -> SetParameter(0, fitFunc->GetParameter(0));
+			fitFunc_1173 -> SetParameter(1, fitFunc->GetParameter(1));
+			fitFunc_1173 -> SetParameter(2, fitFunc->GetParameter(2));
+			fitFunc_1173 -> SetLineColor(kBlack);
+			fitFunc_1173 -> SetLineWidth(2);
+			histo -> Fit(fitFunc_1173,"QRS+"); 
+			res["1.173 MeV"] = std::make_pair(fitFunc_1173->GetParameter(1),fitFunc_1173->GetParameter(2));
+			//histo -> GetXaxis() -> SetRangeUser(0.,3.*fitFunc_1173->GetParameter(1));			
+		}
   //Fitting 1332 keV peak
   if (realPeaks.size() == 2){  
-
-	TF1* fitFunc_1332 = new TF1("fitFunc_1332","gaus",0.95*realPeaks[1],1.05*realPeaks[1]);
-	fitFunc_1332 -> SetLineColor(kBlack);
-	fitFunc_1332 -> SetLineWidth(2);
-	histo -> Fit(fitFunc_1332,"QRS+");		
+		TF1* fitFunc2 = new TF1( "fitFunc2", "gaus", realPeaks[1] -0.5, realPeaks[1] +0.5);
+		histo-> Fit(fitFunc2, "NQR");
+		TF1* fitFunc_1332 = new TF1("fitFunc_1332","gaus",fitFunc2->GetParameter(1)-fitFunc2->GetParameter(2)*0.75, fitFunc2->GetParameter(1)+fitFunc2->GetParameter(2)*0.75);
+			fitFunc_1332 -> SetParameter(0, fitFunc2->GetParameter(0));
+			fitFunc_1332 -> SetParameter(1, fitFunc2->GetParameter(1));
+			fitFunc_1332 -> SetParameter(2, fitFunc2->GetParameter(2));
+			fitFunc_1332 -> SetLineColor(kBlack);
+			fitFunc_1332 -> SetLineWidth(2);
+			histo -> Fit(fitFunc_1332,"QRS+");		
 	
-	res["1.332 MeV"] = std::make_pair(fitFunc_1332->GetParameter(1),fitFunc_1332->GetParameter(2));
+		res["1.332 MeV"] = std::make_pair(fitFunc_1332->GetParameter(1),fitFunc_1332->GetParameter(2));
   
   }
- 
- //Not Co60 spectrum controll
-  if (realPeaks.size() == 0 || realPeaks.size() > 2 ){
-	std::cout << "Errore" << std::endl;
-	res["1.173 MeV"] = std::make_pair(-9999,0);
-  }
+
 
   //Locating and drawing ranges of interest
   if ( realPeaks.size() > 0 && realPeaks.size() < 3 ){
-  	ranges->push_back(res["1.173 MeV"].first-8.*res["1.173 MeV"].second);
-  	ranges->push_back(res["1.173 MeV"].first-5.*res["1.173 MeV"].second);
-	ranges->push_back(res["1.173 MeV"].first-2.*res["1.173 MeV"].second);
-	ranges->push_back(res["1.173 MeV"].first+2.*res["1.173 MeV"].second);
-	ranges->push_back(res["1.173 MeV"].first+5.*res["1.173 MeV"].second);
+		ranges->push_back(res["1.173 MeV"].first-12.*res["1.173 MeV"].second);
+		ranges->push_back(res["1.173 MeV"].first-10.5*res["1.173 MeV"].second);
+		ranges->push_back(res["1.173 MeV"].first-9.*res["1.173 MeV"].second);
+		ranges->push_back(res["1.173 MeV"].first-7.5*res["1.173 MeV"].second);
+		ranges->push_back(res["1.173 MeV"].first-6.*res["1.173 MeV"].second);
+  	ranges->push_back(res["1.173 MeV"].first-4.5*res["1.173 MeV"].second);
+  	ranges->push_back(res["1.173 MeV"].first-3.*res["1.173 MeV"].second);
+		ranges->push_back(res["1.173 MeV"].first-1.5*res["1.173 MeV"].second);
    }	
   if ( realPeaks.size() == 2 ){
-	ranges->push_back(0.5*(res["1.173 MeV"].first+5.*res["1.173 MeV"].second+res["1.332 MeV"].first-5.*res["1.332 MeV"].second));
-	ranges->push_back(res["1.332 MeV"].first-5.*res["1.332 MeV"].second);
-	ranges->push_back(res["1.332 MeV"].first-2.*res["1.332 MeV"].second);
-	ranges->push_back(res["1.332 MeV"].first+2.*res["1.332 MeV"].second);
-   }
-    
-  
-  for(auto range: (*ranges)){
-	TLine* line = new TLine(range,3.,range,histo->GetBinContent(histo->FindBin(range)));
-	line -> SetLineWidth(1);
-	line -> SetLineStyle(7);
-	line -> Draw("same");
-
-
-    
+		ranges->push_back(0.5*(res["1.173 MeV"].first+4.*res["1.173 MeV"].second+res["1.332 MeV"].first-4.*res["1.332 MeV"].second));
+		ranges->push_back(res["1.332 MeV"].first+1.5*res["1.332 MeV"].second);
+		ranges->push_back(res["1.332 MeV"].first+3.*res["1.332 MeV"].second);
   }
-  
+  //Not Co60 spectrum controll
+  if (realPeaks.size() == 0 || realPeaks.size() > 2 ){
+		std::cout << "Errore" << std::endl;
+		res["1.173 MeV"] = std::make_pair(-9999,0);
+  }
+	if( realPeaks.size() == 1 || realPeaks.size() == 2){
+		for(auto range: (*ranges)){
+			TLine* line = new TLine(range,3.,range,histo->GetBinContent(histo->FindBin(range)));
+			line -> SetLineWidth(1);
+			line -> SetLineStyle(7);
+			line -> Draw("same");    
+		}
+	}
   return res;
 }
