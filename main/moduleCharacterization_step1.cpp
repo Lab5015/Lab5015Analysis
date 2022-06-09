590ed741ad59e3ed40cdcd18f4df407877254eb3
cmsdaq <laboratorio5015@gmail.com>
2022-06-09 18:43:33 +0200

    fix for coinc module

--- main/moduleCharacterization_step1.cpp
+++ main/moduleCharacterization_step1.cpp
@@ -268,8 +268,10 @@ int main(int argc, char** argv)
 	  /*for( int iBar = thisBar - 2; iBar < thisBar+3; ++iBar) {                                                                                                                            
 	    if (iBar<0 || iBar>15 ) continue;                                                                                                                                                   
 	    if (iBar == thisBar) continue;
-	    int chL_iext = channelMapping[iBar*2+0];// array0 for coincidence is hard coded... - to be fixed
-	    int chR_iext = channelMapping[iBar*2+1];// array0 for coincidence is hard coded... - to be fixed
+	    //int chL_iext = channelMapping[iBar*2+0];// array0 for coincidence is hard coded... - to be fixed
+	    //int chR_iext = channelMapping[iBar*2+1];// array0 for coincidence is hard coded... - to be fixed
+	    int chL_iext = channelMapping[iBar*2+0]+64;// array1 for coincidence is hard coded... - to be fixed
+	    int chR_iext = channelMapping[iBar*2+1]+64;// array1 for coincidence is hard coded... - to be fixed
 	    float energyL_iext = (*energy)[channelIdx[chL_iext]];              
 	    float energyR_iext = (*energy)[channelIdx[chR_iext]]; 
 	    float totL_iext    = 0.001*(*tot)[channelIdx[chL_iext]];              
@@ -287,8 +289,10 @@ int main(int argc, char** argv)
 
 
 	  for(int iBar = 0; iBar < int(channelMapping.size())/2; ++iBar) {                             
-	    int chL_iext = channelMapping[iBar*2+0];// array0 for coincidence is hard coded... - to be fixed
-	    int chR_iext = channelMapping[iBar*2+1];// array0 for coincidence is hard coded... - to be fixed
+	    //int chL_iext = channelMapping[iBar*2+0];// array0 for coincidence is hard coded... - to be fixed
+	    //int chR_iext = channelMapping[iBar*2+1];// array0 for coincidence is hard coded... - to be fixed
+	    int chL_iext = channelMapping[iBar*2+0]+64;// array1 for coincidence is hard coded... - to be fixed
+	    int chR_iext = channelMapping[iBar*2+1]+64;// array1 for coincidence is hard coded... - to be fixed
 	    float energyL_iext = (*energy)[channelIdx[chL_iext]];              
 	    float energyR_iext = (*energy)[channelIdx[chR_iext]]; 
 	    float totL_iext    = 0.001*(*tot)[channelIdx[chL_iext]];              
@@ -301,8 +305,8 @@ int main(int argc, char** argv)
 	      }
 	    }
 	  }
-	  //if (nActiveBarsArray > 5 ) continue;
-	  if (nActiveBarsArray > 3 ) continue;
+	  if (nActiveBarsArray > 5 ) continue;
+	  //if (nActiveBarsArray > 3 ) continue;
 	}
 	
 	energyL_ext = (*energy)[channelIdx[chL_ext]];
